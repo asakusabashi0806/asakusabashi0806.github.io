7cbbe0dfb88eb4f1763bac685cfa457ab72cab07
nagahamalab0862 <nagahamalab0862@gmail.com>
2026-02-07 18:01:19 +0900

    add js

---
+++ script.js
@@ -0,0 +1,15 @@
+const targets = document.querySelectorAll(".fade-up");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("is-visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+targets.forEach(target => observer.observe(target));
