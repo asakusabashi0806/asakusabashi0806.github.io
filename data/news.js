1c892dfe3690067bde0e21a36475afa476bba048
nagahamalab0862 <nagahamalab0862@gmail.com>
2026-02-08 17:30:03 +0900

    add news

---
+++ data/news.js
@@ -0,0 +1,13 @@
+const newsData = [
+  {
+    date: "2024-09-01",
+    text: "個人サイトを公開しました。"
+  },
+  {
+    date: "2024-08-15",
+    text: "ポートフォリオページを追加しました。",
+    link: "portfolio/"
+  }
+];
+
+window.newsData = newsData;
