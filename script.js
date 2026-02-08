fde58ad2b9d7b0545c47f3a50657921b1bf0e2b7
nagahamalab0862 <nagahamalab0862@gmail.com>
2026-02-08 17:07:33 +0900

    add career

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const header = document.querySelector(".site-header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 20) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
+
 const targets = document.querySelectorAll(".fade-up");
 
 const observer = new IntersectionObserver(
@@ -13,3 +24,27 @@ const observer = new IntersectionObserver(
 );
 
 targets.forEach(target => observer.observe(target));
+
+
+
+const newsList = document.getElementById("news-list");
+
+if (newsList && typeof newsData !== "undefined") {
+  newsData.forEach(item => {
+    const li = document.createElement("li");
+    li.className = "news-item";
+
+    li.innerHTML = `
+      <time class="news-date">${item.date}</time>
+      <div class="news-content">
+        <p>
+          ${item.link
+            ? `<a href="${item.link}">${item.text}</a>`
+            : item.text}
+        </p>
+      </div>
+    `;
+
+    newsList.appendChild(li);
+  });
+};
